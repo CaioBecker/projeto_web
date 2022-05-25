3c9a22d5033289c3c4e5e290f6a94d066df72e7d
Fernando Kuwahara <fernandokoliveira1405@gmail.com>
2022-05-25 03:37:32 -0300

    Tetris Arrumado

--- faculdade/js/script.js
+++ faculdade/js/script.js
@@ -8,7 +8,7 @@ function EasterEgg(){
     if(ConfirmacaoDoEsterEgg == null || ConfirmacaoDoEsterEgg == '')
         alert('Credenciais Não Encontradas!');
     else if(ConfirmacaoDoEsterEgg == 'tetris')
-        window.open('./js/tetris.html');
+        window.open('./html/tetris.html');
     else
         alert('Tente Novamente! (Dica: Digite O Nome Do Jogo)');
 }
